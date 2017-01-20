cbe3616ecd5c5399eb691b5961fe3e26cfe21e89
khines <khines@hbk.com>
2017-01-20 16:46:53 -0600

    Finish modules 4 and 5

---
+++ TerraformUpAndRunning/4 - Modules/global/s3/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 0.8, < 0.9"
+}
+
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${var.bucket_name}"
+
+	versioning {
+		enabled = true
+	}
+
+	lifecycle {
+		prevent_destroy = true
+	}
+
+	tags {
+		BU = "it"
+		Consumer = "IT"
+		Creator = "Kristofor Hines"
+		Owner = "IT"
+		ENV = "devuseast1"
+	}
+}
